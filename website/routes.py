8b9def43e9fcde6b781f28fafe5c05de0a72f2ab
rpdavila <rafael.pietri@gmail.com>
2021-02-24 17:00:17 -0500

    reverted back to old version

--- website/routes.py
+++ website/routes.py
@@ -1,6 +1,7 @@
-from website import app
-from flask import render_template
+from website import app, mail
+from flask import render_template, request, redirect, url_for
 from website.forms import ContactForm
+from flask_mail import Message
 
 
 @app.route('/')
@@ -12,10 +13,20 @@ def home():
 @app.route('/contact', methods=["GET", "POST"])
 def contact():
     form = ContactForm()
-    if form.validate_on_submit():
-        return f'name: {form.name.data}, email: {form.email.data}, subject: {form.subject.data}, ' \
-               f'message: {form.body.data}'
+    if form.validate_on_submit() and form.subject.data == "Constructive Feedback" or "Send Resume for Possible Hire"\
+            or "Internship Opportunity":
+        if request.method == "POST":
+            if form.validate_on_submit():
+                return redirect(url_for('home'))
     return render_template('contact.html', form=form, title='Contact')
+
+
+def send_email(form):
+    msg = Message(subject=f'{form.subject.data}',
+                  sender=f'{form.email.data}',
+                  recipients=['rafael.pietri@gmail.com'])
+    msg.body = f'Hello my name is {form.name.data},\n{form.body.data}'
+    mail.send(msg)
 
 
 @app.route('/projects')
