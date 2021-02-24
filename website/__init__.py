177854ec8cdab475f692b9a42ded8b8e36118faa
rpdavila <rafael.pietri@gmail.com>
2021-02-24 10:28:00 -0500

    modified os.environment.get to os.getenv for easy redability in init file and added environ variable to secret key

--- website/__init__.py
+++ website/__init__.py
@@ -7,8 +7,8 @@ app.config['SECRET_KEY'] = os.getenv('portfolio')
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('Email_User')
-app.config['MAIL_PASSWORD'] = os.environ.get('Email_Pass')
+app.config['MAIL_USERNAME'] = os.getenv('Email_User')
+app.config['MAIL_PASSWORD'] = os.getenv('Email_Pass')
 mail = Mail(app)
 
 from website import routes
