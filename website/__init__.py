18a27fcf3a5753375d9e110df0d65100829b5b17
rpdavila <rafael.pietri@gmail.com>
2021-02-24 16:51:17 -0500

    added sqlalchemy to init file

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,13 @@
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
+
 
 app = Flask(__name__, template_folder='./templates')
 app.config['SECRET_KEY'] = os.getenv('portfolio')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////website/db/website.sqlite'
+db = SQLAlchemy(app)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
