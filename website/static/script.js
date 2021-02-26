ed819089ae18bc603ce8c275c8901c1b88c4a755
rpdavila <rafael.pietri@gmail.com>
2021-02-26 17:01:04 -0500

    added venv folder to git ignore. Added jscript grid cards and made them into clickable cards, added cursor change for cards when maude hover to pointer

---
+++ website/static/script.js
@@ -0,0 +1,17 @@
+let card_1 = document.getElementById('card-1')
+let card_2 = document.getElementById('card-2')
+let card_3 = document.getElementById('card-3')
+
+
+card_1.addEventListener('click', function() {
+    window.location.href = "https://github.com/rpdavila/Personal_Portfolio";
+} )
+
+card_2.addEventListener('click', function() {
+    window.location.href = "https://github.com/rpdavila/image-ai";
+} )
+
+card_3.addEventListener('click', function() {
+    window.location.href = "https://github.com/zero-to-mastery/ascii-art";
+} )
+
