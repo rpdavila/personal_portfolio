ddbb9ec181b6c57da8ed97847d1611c83722ff8b
rpdavila <rafael.pietri@gmail.com>
2021-07-14 13:52:18 -0400

    added client project card

--- website/static/script.js
+++ website/static/script.js
@@ -3,6 +3,7 @@ let card_1 = document.getElementById('card-1');
 let card_2 = document.getElementById('card-2');
 let card_3 = document.getElementById('card-3');
 let card_4 = document.getElementById('card-4');
+let card_5 = document.getElementById('card-5');
 
 card_1.addEventListener('click', function() {
     window.location.href = "https://github.com/rpdavila/personal_portfolio";
@@ -18,4 +19,8 @@ card_3.addEventListener('click', function() {
 
 card_4.addEventListener('click', function() {
     window.location.href = "https://github.com/rpdavila/twitter_analytics";
+} );
+
+card_5.addEventListener('click', function() {
+    window.open("https://github.com/rpdavila/OR_webapp");
 } );
